eeb3c5d69cb1a6d32a79203fd132c9034ad72aaa
optimumsec <0xoptimum@gmail.com>
2024-09-28 15:58:17 +0300

    lil fixes to JackpotOrHoneypot3

--- src/JackpotOrHoneypot3.sol
+++ src/JackpotOrHoneypot3.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.20;
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
-// Assume correctness of the IReceiver contract implementation
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 interface IReceiver {
     // stores the owner of the receiver contract, can only be called once
     function init(address _owner) external payable;
@@ -12,7 +12,7 @@ interface IReceiver {
 // The contract will clone a Receiver contract for the winner (by using ReceiverFactory which relies on the Clones
 // library)
 // and will send the prize to this address. The prize will be claimable only by the _owner defined during deposit.
-contract JackpotOrHoneypot3 {
+contract JackpotOrHoneypot3 is ReentrancyGuard {
     mapping(address => address) public depositsOwners;
     uint256 initTime;
     bool prizePaid;
@@ -20,9 +20,10 @@ contract JackpotOrHoneypot3 {
     constructor(ReceiverFactory _receiverFactory) payable {
         require(msg.value == 10 ether);
         initTime = block.timestamp;
+        // Assume receiverFactory is initialized correctly
         receiverFactory = _receiverFactory;
     }
-    function deposit(address _owner) external payable {
+    function deposit(address _owner) external payable nonReentrant {
         require(_owner != address(0));
         require(block.timestamp <= initTime + 7 days, "deposit phase ended");
         require(msg.value == 1 ether);
@@ -32,34 +33,36 @@ contract JackpotOrHoneypot3 {
         );
         depositsOwners[msg.sender] = _owner;
     }
-    function payPrize() external {
+    function payPrize() external nonReentrant {
         require(
             block.timestamp > initTime + 7 days,
             "withdraw phase has not begun yet"
         );
-        require(block.timestamp < initTime + 8 days, "withdraw phase is over");
         address owner = depositsOwners[msg.sender];
         require(owner != address(0), "no deposit made");
         uint256 toPay = 1 ether;
-
         if (!prizePaid) {
             toPay += 10 ether;
             prizePaid = true;
         }
         depositsOwners[msg.sender] = address(0);
-        receiverFactory.sendPrizeToReceiverContract(owner, toPay);
+        address receiver = receiverFactory.createReceiverContract(owner);
+        payable(receiver).transfer(toPay);
     }
 }
 contract ReceiverFactory {
-    address receiverImplementation;
-    constructor(address _implementation) {
+    IReceiver receiverImplementation;
+    // Assume correctness of the IReceiver contract implementation
+    constructor(IReceiver _implementation) {
         receiverImplementation = _implementation;
     }
-    function sendPrizeToReceiverContract(address _owner, uint256 toPay) public {
-        address receiver = Clones.cloneDeterministic(
-            receiverImplementation,
+    function createReceiverContract(
+        address _owner
+    ) public returns (address receiver) {
+        receiver = Clones.cloneDeterministic(
+            address(receiverImplementation),
             keccak256(abi.encode(_owner))
         );
-        IReceiver(receiver).init{value: toPay}(_owner);
+        IReceiver(receiver).init(_owner);
     }
 }
