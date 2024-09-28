c5054387cdbf89952452de7558a06f17845aaff6
optimumsec <0xoptimum@gmail.com>
2024-09-28 16:47:56 +0300

    lil changes made to JackpotOrHoneypot3

--- src/JackpotOrHoneypot3.sol
+++ src/JackpotOrHoneypot3.sol
@@ -7,8 +7,8 @@ interface IReceiver {
     // Allows only the owner defined in the init function to claim the contract balance
     function pullRewards() external;
 }
-// The prize is 10 eth, anyone can deposit during the deposit phase which lasts 7 days.
-// After that, anyone can call payPrize, the first one to call this function will be able to claim the prize.
+// participants deposit 1 eth during the deposit phase which lasts 7 days.
+// After that, the first depositor to call payPrize will be able to claim the all previous deposits.
 // The contract will clone a Receiver contract for the winner (by using ReceiverFactory which relies on the Clones
 // library)
 // and will send the prize to this address. The prize will be claimable only by the _owner defined during deposit.
@@ -17,8 +17,7 @@ contract JackpotOrHoneypot3 is ReentrancyGuard {
     uint256 initTime;
     bool prizePaid;
     ReceiverFactory receiverFactory;
-    constructor(ReceiverFactory _receiverFactory) payable {
-        require(msg.value == 10 ether);
+    constructor(ReceiverFactory _receiverFactory) {
         initTime = block.timestamp;
         // Assume receiverFactory is initialized correctly
         receiverFactory = _receiverFactory;
@@ -34,20 +33,17 @@ contract JackpotOrHoneypot3 is ReentrancyGuard {
         depositsOwners[msg.sender] = _owner;
     }
     function payPrize() external nonReentrant {
+        require(!prizePaid);
         require(
             block.timestamp > initTime + 7 days,
             "withdraw phase has not begun yet"
         );
         address owner = depositsOwners[msg.sender];
         require(owner != address(0), "no deposit made");
-        uint256 toPay = 1 ether;
-        if (!prizePaid) {
-            toPay += 10 ether;
-            prizePaid = true;
-        }
+        prizePaid = true;
         depositsOwners[msg.sender] = address(0);
         address receiver = receiverFactory.createReceiverContract(owner);
-        payable(receiver).transfer(toPay);
+        payable(receiver).transfer(address(this).balance);
     }
 }
 contract ReceiverFactory {
